e921536cb8b0e816dc2352ae040f841bc3faebce
Julian Dunning <julian.dunning@bba11.mccombs.utexas.edu>
2015-04-15 20:24:11 -0500

    cross

--- Crosstabs.R
+++ Crosstabs.R
@@ -4,47 +4,41 @@ df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/
 
 spread(df, DNAME, N)
 
-# Basic Tableau Table Calculations
-# http://onlinehelp.tableau.com/current/pro/online/mac/en-us/calculations_tablecalculations_definebasic.html
+# RANK - Popular crime
 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"SELECT CRIME_TYPE, COUNT(CRIME_TYPE), rank() 
-OVER (order by COUNT(CRIME_TYPE) desc) as RANK 
-FROM APD_INCIDENTS order by 2,3 desc"
+"select CRIME_TYPE , count(*) n from APD_INCIDENTS group by CRIME_TYPE ORDER by N desc"
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/ORCL.usuniversi01134.oraclecloud.internal', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
+
+spread(df, DNAME, N)
+
+#Difference - from last Duration of peace
+
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
+"select CRIME_TYPE, DATE_I, TIME, LAG(TIME)
+OVER (PARTITION BY DATE_I order by TIME) Prev
+OVER (PARTITION BY DATE_I order by TIME) - TIME
+
+from 
+(SELECT CRIME_TYPE, DATE_I, TIME FROM APD_INCIDENTS ORDER BY DATE_I,TIME)
+"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/ORCL.usuniversi01134.oraclecloud.internal', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"SELECT CRIME_TYPE, COUNT(CRIME_TYPE) as count, rank() 
-OVER (order by count desc) as DEPT_RANK 
-FROM APD_INCIDENTS "
+"SELECT DATE_I,TIME, CRIME_TYPE, nth_value(CRIME_TYPE, 3)
+OVER (PARTITION BY DATE_I) CRIME_TYPE
+FROM
+(SELECT DATE_I, TIME, CRIME_TYPE
+FROM APD_INCIDENTS ORDER BY DATE_I,CAST(TIME as INT))
+"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/ORCL.usuniversi01134.oraclecloud.internal', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
 
+#CUME_DIST
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"SELECT * FROM (SELECT empno, deptno, sal, rank() 
-OVER (PARTITION BY deptno order by sal desc) as DEPT_RANK 
-FROM emp) where DEPT_RANK = 1 "
+"select CRIME_TYPE, n, cume_dist() 
+OVER (order by n) cume_dist
+from 
+(SELECT CRIME_TYPE, count(*) n from APD_INCIDENTS group by CRIME_TYPE)
+order by n desc"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/ORCL.usuniversi01134.oraclecloud.internal', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
 
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"select empno, deptno, sal, last_value(max_sal) 
-OVER (PARTITION BY deptno order by sal) max_sal, last_value(max_sal) 
-OVER (PARTITION BY deptno order by sal) - sal sal_diff
-from
-(SELECT empno, deptno, sal, max(sal)
-OVER (PARTITION BY deptno) max_sal 
-FROM emp) 
-order by 2,3 desc"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/ORCL.usuniversi01134.oraclecloud.internal', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"SELECT empno, deptno, sal, nth_value(sal, 2)
-OVER (PARTITION BY deptno) max_sal 
-FROM emp
-order by 2,3 desc"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_Scott', PASS='orcl', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
-
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"select empno, deptno, sal, cume_dist() 
-OVER (PARTITION BY deptno order by sal) cume_dist
-from emp 
-order by 2,3 desc"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_Scott', PASS='orcl', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
