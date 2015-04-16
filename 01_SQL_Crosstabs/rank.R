ea83380ac81c10cc63bac8fab8fb4846fc37dbbc
Sarah Stancik <skstancik@utexas.edu>
2015-04-16 18:59:10 -0500

    clean rank file

--- 01_SQL_Crosstabs/rank.R
+++ 01_SQL_Crosstabs/rank.R
@@ -1,8 +1,3 @@
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"SELECT * FROM APD_INCIDENTS"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/ORCL.usuniversi01134.oraclecloud.internal', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
-
-
 # RANK - Popular crime
 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
